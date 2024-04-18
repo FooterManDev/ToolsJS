1dca1d363d31e614f759e3e41b689a230ef70de3
footerman <footerman.work@gmail.com>
2024-04-17 23:59:59 -0400

    backpacks

--- src/main/java/io/github/toolsjs/kubejs/ToolsJSPlugin.java
+++ src/main/java/io/github/toolsjs/kubejs/ToolsJSPlugin.java
@@ -5,6 +5,7 @@ import dev.latvian.mods.kubejs.registry.RegistryInfo;
 import io.github.toolsjs.ToolsJS;
 import dev.latvian.mods.kubejs.KubeJSPlugin;
 import io.github.toolsjs.kubejs.builders.*;
+import io.github.toolsjs.kubejs.builders.sophisticated.BackpackBuilder;
 
 @SuppressWarnings("SpellCheckingInspection")
 public class ToolsJSPlugin extends KubeJSPlugin {
@@ -20,7 +21,9 @@ public class ToolsJSPlugin extends KubeJSPlugin {
             RegistryInfo.ITEM.addType("wateringcan", WateringcanItemBuilder.class, WateringcanItemBuilder::new);
         }
 
-        if (Platform.isModLoaded("sophisticatedbackpacks"))
+        if (Platform.isModLoaded("sophisticatedbackpacks")) {
+            RegistryInfo.ITEM.addType("sophisticated_backpack", BackpackBuilder.class, BackpackBuilder::new);
+        }
         RegistryInfo.ITEM.addType("horse_armor", HorsearmorItemBuilder.class, HorsearmorItemBuilder::new);
         RegistryInfo.ITEM.addType("shield", ShieldBuilder.class, ShieldBuilder::new);
         ToolsJS.LOGGER.info("This is my KubeJS Plugin!");
