181b4dbd0b19b9458e4002e909dbab878532301d
blue <bluemethyst@gmail.com>
2024-04-30 22:25:09 +1200

    i need hlep

--- src/main/kotlin/io/github/toolsjs/ToolsJS.kt
+++ src/main/kotlin/io/github/toolsjs/ToolsJS.kt
@@ -3,9 +3,14 @@ package io.github.toolsjs
 import net.minecraftforge.fml.common.Mod
 import org.apache.logging.log4j.LogManager
 import org.apache.logging.log4j.Logger
+import io.github.toolsjs.common.AnnouncementChecker
+import net.minecraftforge.common.MinecraftForge
 
 @Mod(ToolsJS.ID)
 object ToolsJS {
     const val ID = "toolsjs"
     val LOGGER: Logger = LogManager.getLogger()
+    init {
+        MinecraftForge.EVENT_BUS.register(AnnouncementChecker::class.java)
+    }
 }
