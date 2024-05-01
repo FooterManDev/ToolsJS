7b27d0e7d42a4fbc4999edfd8480512ea0d61aee
blue <bluemethyst@gmail.com>
2024-05-02 11:17:45 +1200

    wait it works?

--- src/main/kotlin/io/github/toolsjs/ToolsJS.kt
+++ src/main/kotlin/io/github/toolsjs/ToolsJS.kt
@@ -1,16 +1,16 @@
 package io.github.toolsjs
 
+import dev.architectury.event.events.common.PlayerEvent
 import net.minecraftforge.fml.common.Mod
 import org.apache.logging.log4j.LogManager
 import org.apache.logging.log4j.Logger
 import io.github.toolsjs.common.AnnouncementChecker
-import net.minecraftforge.common.MinecraftForge
 
 @Mod(ToolsJS.ID)
 object ToolsJS {
     const val ID = "toolsjs"
     val LOGGER: Logger = LogManager.getLogger()
     init {
-        MinecraftForge.EVENT_BUS.register(AnnouncementChecker::class.java)
+        PlayerEvent.PLAYER_JOIN.register(AnnouncementChecker::onPlayerLogin)
     }
 }
--- src/main/kotlin/io/github/toolsjs/common/AnnouncementChecker.kt
+++ src/main/kotlin/io/github/toolsjs/common/AnnouncementChecker.kt
@@ -1,21 +1,42 @@
 package io.github.toolsjs.common
 
-import net.minecraftforge.eventbus.api.SubscribeEvent
-import net.minecraftforge.fml.common.Mod
+import kotlinx.coroutines.*
 import java.net.URL
 import io.github.toolsjs.ToolsJS
 import net.minecraft.network.chat.Component
-import net.minecraftforge.event.entity.player.PlayerEvent
+import net.minecraft.server.level.ServerPlayer
+import net.minecraftforge.fml.common.Mod
+import com.google.gson.Gson
+
+data class Announcement(
+    val importance: String,
+    val title: String,
+    val message: String
+)
 
 @Mod.EventBusSubscriber(modid = ToolsJS.ID)
 class AnnouncementChecker {
     companion object {
-        @SubscribeEvent
-        fun onPlayerLogin(event: PlayerEvent.PlayerLoggedInEvent) {
-            val url = URL("https://raw.githubusercontent.com/Footermandev/ToolsJS/master/announcement.json")
-            val announcement = url.readText()
-            val player = event.entity
-            player.sendSystemMessage(Component.literal("woah announcement babe $announcement"))
+        @OptIn(DelicateCoroutinesApi::class)
+        fun onPlayerLogin(player: ServerPlayer) {
+            GlobalScope.launch {
+                val url = URL("https://raw.githubusercontent.com/Footermandev/ToolsJS/master/announcement.json")
+                val rawAnnouncement = withContext(Dispatchers.IO) { url.readText() }
+                val gson = Gson()
+                val announcement: Announcement = gson.fromJson(rawAnnouncement, Announcement::class.java)
+                if (announcement.importance.isNotEmpty() && announcement.title.isNotEmpty() && announcement.message.isNotEmpty()) {
+                    if (announcement.importance.lowercase() == "severe") {
+                        player.sendSystemMessage(Component.literal("IMPORTANT: ${announcement.message}"))
+                    } else if (announcement.importance.lowercase() == "misc") {
+                        player.sendSystemMessage(Component.literal("MISC: ${announcement.message}"))
+                    } else if (announcement.importance.lowercase() == "debug") {
+                        player.sendSystemMessage(Component.literal(announcement.message))
+                    }
+                } else {
+                    ToolsJS.LOGGER.warn("Failed to parse announcement, report to Footerman or Blue")
+                    //ToolsJS.LOGGER.warn("Failed to parse announcement JSON! Some dumbass fucked it and doesn't know how to write simple FUCKING JSON, FUCK MAN. report to footer or blue uwu :3")
+                }
+            }
         }
     }
 }
